b1e5ffeafa2aaf9833efc1f237b9a5ef6e9f01b9
pedrohelias <pedrohelias95@hotmail.com>
2025-05-08 10:00:10 -0300

    maps complexos

--- basics/e04_sacolaMercado2.go
+++ basics/e04_sacolaMercado2.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type product struct { //para fazer um map mais complexo
+	qtd int
+	code string
+	isAlive bool
+	valor float64
+}
 
 func listaProdutos(produtos map[string]float64){
 	counter := 0
@@ -13,8 +19,37 @@ func listaProdutos(produtos map[string]float64){
 	fmt.Println("______________________")
 }
 
+func listaProdutosComplex(produtos map[string]product){
+	counter := 0
+	fmt.Println("Tabela de Produtos")
+	for chave, valor := range produtos {
+		counter++
+		fmt.Printf("%d - %s, preço: R$ %.2f, quantidade: %d, válido? %v\n", counter, chave, valor.valor, valor.qtd, valor.isAlive)
+	}
+	fmt.Println("______________________")
+}
+
 func addProdutos(produto string, preco float64, produtos map[string]float64) map[string]float64{
 	produtos[produto] = preco
+	return produtos
+}
+
+func removerProdutos(produtos map[string]float64, chaveRem string) map[string]float64{
+	for chave:= range produtos {
+		if (chave == chaveRem){
+			delete(produtos, chaveRem)
+		}
+
+	}
+	return produtos
+}
+
+func atualizaValor(produtos map[string]float64, chaveA string, valorA float64) map[string]float64{
+	for chave := range produtos{
+		if chave == chaveA {
+			produtos[chave] = valorA
+		}
+	}
 	return produtos
 }
 
@@ -30,9 +65,30 @@ func main(){
 		"laranja":5.56,
 	}
 
+	produtosComplexos := map[string]product{ //aqui vai permitir que tenhamos objetos e construções mais complexas
+		"uva":{
+			qtd: 3,
+			code: "12345",
+			isAlive: true,
+			valor: 14.90,
+		},
+		"maca":{
+			qtd: 4,
+			code: "54321",
+			isAlive: false,
+			valor: 12.00,
+		},
+	}
+
 	listaProdutos(produtos)
 	produtos = addProdutos(fruta, preco, produtos)
 	listaProdutos(produtos)
+	produtos = removerProdutos(produtos, "uva")
+	listaProdutos(produtos)
+	produtos = atualizaValor(produtos, "laranja", 4.60)
+	listaProdutos(produtos)
+	listaProdutosComplex(produtosComplexos)
+
 
 
 	fmt.Println("teste")
